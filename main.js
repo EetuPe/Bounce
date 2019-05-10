32c4d5a582bfad379286df76abdbb2791f9594bf
dkrpv <35373444+dkrpv@users.noreply.github.com>
2019-05-10 15:09:30 +0300

    Fixed indentation, spikes are normal

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var ball;   //red ball
+var ball;	//red ball
 var ball2; //blue ball
 var floor = [];
 var key,pos=0;
@@ -22,71 +22,67 @@ window.onload = function () {
   let posXFloor = 0;
   let posYFloor = 0;
 	var ctx = canvas.getContext('2d');
-  for(i = 0; i < 9; i++)
-    {
-      floor[i] = new Image();
-      floor[i].src = "Bricks.png";
-      floor[i].onload=function()
-      {
-        ctx.drawImage(floor[i],i*50,posYFloor);
-      }
+  for(i = 0; i < 9; i++) {
+    floor[i] = new Image();
+    floor[i].src = "Bricks.png";
+    floor[i].onload=function() {
+      ctx.drawImage(floor[i],i*50,posYFloor);
     }
+  }
 
-		ball.onload=function()
-		{
-  		ctx.drawImage(ball,posX,posY);
-		}
-
-		ball2.onload=function()
-		{
-  		ctx.drawImage(ball2,posX2,posY2);
-		}
-
-		spikes.onload=function() {
-			ctx.drawImage(spikes,posSpikeX,posSpikeY); //temporary positioning for dar spikes
-		}
-
-		document.onkeydown=function(e)
-		{
-  		pos=1;
-  		key=window.event?e.keyCode:e.which;
-		}
-		document.onkeyup=function(e){pos=0;}
-		setInterval(function()
-		{
-  		if(pos==0)return;
-  		if(key==65)posX-=2;
-  		if(key==87)posY-=2;
-  		if(key==68)posX+=2;
-  		if(key==83)posY+=2;
-
-  		if(key==37)posX2-=2;
-  		if(key==38)posY2-=2;
-  		if(key==39)posX2+=2;
-  		if(key==40)posY2+=2;
-  		canvas.width=canvas.width;
-  		ctx.drawImage(ball,posX,posY);
-  		ctx.drawImage(ball2,posX2,posY2);
-		},5);
+	ball.onload=function() {
+		ctx.drawImage(ball,posX,posY);
 	}
 
+	ball2.onload=function() {
+		ctx.drawImage(ball2,posX2,posY2);
+	}
+
+	spikes.onload=function() {
+		ctx.drawImage(spikes,posSpikeX,posSpikeY);
+	}
+
+	document.onkeydown=function(e) {
+		pos=1;
+		key=window.event?e.keyCode:e.which;
+	}
+
+	document.onkeyup=function(e) {
+		pos=0;
+	}
+
+	setInterval(function() {
+		if(pos==0)return;
+		if(key==65)posX-=2;
+		if(key==87)posY-=2;
+		if(key==68)posX+=2;
+		if(key==83)posY+=2;
+
+		if(key==37)posX2-=2;
+		if(key==38)posY2-=2;
+		if(key==39)posX2+=2;
+		if(key==40)posY2+=2;
+		canvas.width=canvas.width;
+		ctx.drawImage(ball,posX,posY);
+		ctx.drawImage(ball2,posX2,posY2);
+	},5);
+}
 
 function gameStart() {
 
 }
 
 function gameChar1(width, height, color, x, y, type) {
-    ball.type = type;
-    ball.width = width;
-    ball.height = height;
-    ball.x = x;
-    ball.y = y;
-    ball.speedX = 0;
-    ball.speedY = 0;
-    ball.gravity = 0.1;
-    ball.gravitySpeed = 0;
-    ball.bounce = 1;
-
+  ball.type = type;
+  ball.width = width;
+  ball.height = height;
+  ball.x = x;
+  ball.y = y;
+  ball.speedX = 0;
+  ball.speedY = 0;
+  ball.gravity = 0.1;
+  ball.gravitySpeed = 0;
+  ball.bounce = 1;
 }
 
 gameStart();
